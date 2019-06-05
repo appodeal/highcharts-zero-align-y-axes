9bacb307930f6450f3f3d8153b4ee96a7912f750
alex-skakun <alex.skakun@me.com>
2019-06-05 14:03:58 +0300

    fixed issue with package.json in build script

--- tools/dist-files.js
+++ tools/dist-files.js
@@ -10,7 +10,7 @@ let files = fs.readdirSync(process.cwd()),
     ignored = fs.readFileSync(path.resolve(process.cwd(), '.npmignore')).toString().split('\n');
 
 for (let fileName of files) {
-    if (!ignored.includes(fileName)) {
+    if (!ignored.includes(fileName) && fileName !== 'package.json') {
         fs.copyFileSync(fileName, `./dist/${fileName}`);
     }
 }
